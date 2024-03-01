7a2f965392da34b4ae31b17b5092a05e5c437bd4
Vladislav Andreev <andreevvlad443@gmail.com>
2024-03-01 18:46:10 +0300

    fix: entrypoint remove useless logging

--- application-frontend/src/main.ts
+++ application-frontend/src/main.ts
@@ -1,7 +1,6 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser'
+import { appConfig } from './app/app.config'
+import { AppComponent } from './app/app.component'
 
 bootstrapApplication(AppComponent, appConfig)
-  .then((b) => console.log(b))
   .catch((err) => console.error(err));
