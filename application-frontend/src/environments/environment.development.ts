f62bca36a6d0627287cca3cac645bc5eb5499b1e
Vladislav Andreev <andreevvlad443@gmail.com>
2024-03-02 23:03:18 +0300

    fix: development environment conf updated in compliance with production one

--- application-frontend/src/environments/environment.development.ts
+++ application-frontend/src/environments/environment.development.ts
@@ -1,25 +1,49 @@
 import {AuthConfig} from 'angular-oauth2-oidc';
 
-const issuerUri: string = 'http://localhost:9000/realms/lab4-web'
+/*
+  OAUTH2 IDENTITY PROVIDER CONFIGURATION
+ */
+const IDENTITY_PROVIDER_PROTOCOL: string = 'http'
+const IDENTITY_PROVIDER_HOST: string = 'localhost'
+const IDENTITY_PROVIDER_PORT: string = '9000'
+const IDENTITY_PROVIDER_KEYCLOAK_REALM: string = 'lab4-web'
+const IDENTITY_PROVIDER_CLIENT: string = 'lab4-web-client'
+const IDENTITY_PROVIDER_AUTHORIZATION_FLOW: string = 'code'
+const IDENTITY_PROVIDER_SCOPES: string = 'profile email'
+const IDENTITY_PROVIDER_REDIRECT_URI: string = window.location.origin
+const IDENTITY_PROVIDER_ISS_URI: string = `${IDENTITY_PROVIDER_PROTOCOL}://${IDENTITY_PROVIDER_HOST}:${IDENTITY_PROVIDER_PORT}/realms/${IDENTITY_PROVIDER_KEYCLOAK_REALM}`
+
+/*
+  API CONFIGURATION
+ */
+const ATTEMPT_API_PROTOCOL: string = 'http'
+const ATTEMPT_API_HOST: string = 'localhost'
+const ATTEMPT_API_PORT: string = '8090'
+const ATTEMPT_API_VERSION: string = 'v1'
+
+/*
+  DO NOT MODIFY THE FOLLOWING CODE BLOCKS
+ */
+const PRODUCTION_MODE: boolean = false
 
 const oauthDevConfig: AuthConfig = {
   preserveRequestedRoute: true,
-  logoutUrl: issuerUri + '/protocol/openid-connect/logout',
-  issuer: issuerUri,
-  tokenEndpoint: issuerUri + '/protocol/openid-connect/token',
-  userinfoEndpoint: issuerUri + '/protocol/openid-connect/userinfo',
-  revocationEndpoint: issuerUri + '/protocol/openid-connect/revoke',
-  clientId: 'lab4-web-client',
-  responseType: 'code',
-  scope: 'profile email',
-  showDebugInformation: true,
+  logoutUrl: `${IDENTITY_PROVIDER_ISS_URI}/protocol/openid-connect/logout`,
+  issuer: IDENTITY_PROVIDER_ISS_URI,
+  tokenEndpoint: `${IDENTITY_PROVIDER_ISS_URI}/protocol/openid-connect/token`,
+  userinfoEndpoint: `${IDENTITY_PROVIDER_ISS_URI}/protocol/openid-connect/userinfo`,
+  revocationEndpoint: `${IDENTITY_PROVIDER_ISS_URI}/protocol/openid-connect/revoke`,
+  clientId: IDENTITY_PROVIDER_CLIENT,
+  responseType: IDENTITY_PROVIDER_AUTHORIZATION_FLOW,
+  scope: IDENTITY_PROVIDER_SCOPES,
   useSilentRefresh: false,
-  redirectUri: 'http://localhost:8888/',
+  redirectUri: IDENTITY_PROVIDER_REDIRECT_URI,
+  showDebugInformation: !PRODUCTION_MODE,
   requireHttps: false
 }
 
 export const environment = {
-  production: false,
-  attemptApiBase: 'http://localhost:8090/api/v1',
-  oauthConfig: oauthDevConfig
+  production: PRODUCTION_MODE,
+  oauthConfig: oauthDevConfig,
+  attemptApiBase: `${ATTEMPT_API_PROTOCOL}://${ATTEMPT_API_HOST}:${ATTEMPT_API_PORT}/api/${ATTEMPT_API_VERSION}`
 };
