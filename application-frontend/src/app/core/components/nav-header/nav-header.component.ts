33d4748526aa05d76f8b619c5efc79a2f8bb48d4
Vladislav Andreev <andreevvlad443@gmail.com>
2024-03-06 23:02:15 +0300

    feat: navigation header component

---
+++ application-frontend/src/app/core/components/nav-header/nav-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  standalone: true,
+  selector: 'nav-header',
+  templateUrl: './nav-header.component.html',
+  styleUrl: './nav-header.component.css'
+})
+export class NavHeaderComponent {
+}
