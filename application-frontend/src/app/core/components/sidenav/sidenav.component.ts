1c41270ec12a05667788916ff112e30469534fc0
Vladislav Andreev <andreevvlad443@gmail.com>
2024-03-06 23:05:44 +0300

    feat: global application navigation component

---
+++ application-frontend/src/app/core/components/sidenav/sidenav.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field'
+import { MatSelectModule } from '@angular/material/select'
+import { MatButtonModule } from '@angular/material/button'
+import { MatToolbar } from '@angular/material/toolbar'
+import { NgIf } from '@angular/common'
+import { MatIconModule } from '@angular/material/icon'
+import { NavItemComponent } from '../nav-item/nav-item.component'
+import { NavHeaderComponent } from '../nav-header/nav-header.component'
+import { NavIconItemComponent } from '../nav-icon-item/nav-icon-item.component'
+import { Router } from '@angular/router'
+import { AuthService } from '../../services/auth.service'
+
+@Component({
+  selector: 'app-sidenav',
+  standalone: true,
+  imports: [
+    MatFormField,
+    MatToolbar,
+    NgIf,
+    MatIconModule,
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    NavItemComponent,
+    NavHeaderComponent,
+    NavIconItemComponent
+  ],
+  templateUrl: './sidenav.component.html',
+  styleUrl: './sidenav.component.css'
+})
+export class SidenavComponent {
+
+  constructor(
+      private readonly router: Router,
+      private readonly authService: AuthService
+  ) {}
+
+  isAuthenticated(): boolean {
+    return this.authService.hasValidToken()
+  }
+
+  login(): void {
+    this.authService.login()
+  }
+
+  logout(): void {
+    this.authService.logout()
+  }
+
+  homeRedirect(): void {
+    this.router.navigate(['/home'])
+  }
+
+  panelRedirect(): void {
+    this.router.navigate(['/panel'])
+  }
+}
