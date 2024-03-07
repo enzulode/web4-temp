a06d5d7987c9ca572e0da78d58619101edad8c08
Vladislav Andreev <andreevvlad443@gmail.com>
2024-03-07 16:40:53 +0300

    fix: useless responsiveness removed & dead stylesheet removed

--- application-frontend/src/app/features/home/home.component.ts
+++ application-frontend/src/app/features/home/home.component.ts
@@ -1,62 +1,10 @@
-import { Component, OnInit } from '@angular/core'
-import { MatButton } from '@angular/material/button'
-import { MatToolbar } from '@angular/material/toolbar'
-import { AuthService } from '../../core/services/auth.service'
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
-import { NgIf } from '@angular/common'
-import { Router } from '@angular/router'
-import { SidenavComponent } from '../../core/components/sidenav/sidenav.component'
+import { Component } from '@angular/core'
 
 @Component({
 	selector: 'app-home',
 	standalone: true,
-  imports: [
-    MatButton,
-    MatToolbar,
-    NgIf,
-    SidenavComponent
-  ],
 	templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
-
-  isMobile: boolean = false
-  isTablet: boolean = false
-  isDesktop: boolean = true
-
-  constructor(
-      private readonly authService: AuthService,
-      private readonly responsive: BreakpointObserver,
-      private readonly router: Router
-  ) {}
-
-  ngOnInit(): void {
-    this.responsive.observe([Breakpoints.XSmall, Breakpoints.Small])
-    .subscribe(result => {
-      this.isMobile = result.matches;
-    })
-
-    this.responsive.observe(Breakpoints.Medium)
-    .subscribe(result => {
-      this.isTablet = result.matches
-    })
-
-    this.responsive.observe([Breakpoints.Large, Breakpoints.XLarge])
-    .subscribe(result => {
-      this.isDesktop = result.matches
-    })
-  }
-
-  redirectToPanel(): void {
-    this.router.navigateByUrl('/panel')
-  }
-
-  login(): void {
-    this.authService.login()
-  }
-
-  isLoggedIn(): boolean {
-    return this.authService.hasValidToken()
-  }
+export class HomeComponent {
 }
